1353b8c8e04fe956173d4aede9fd90ecdee46e87
Filip Sunnemar <filipsunnemar@hotmail.com>
2020-02-23 16:41:23 +0100

    Increment, comments and Reset

--- src/Main.elm
+++ src/Main.elm
@@ -11,9 +11,18 @@ import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
+--Hierarki, Elm arkitektur.
+-- Vänta på user input med exempelvis onClick
+-- Skicka ett Msg (meddelande) till update
+-- Producera en ny model
+-- Kalla view för att få en ny HTML
+-- Visa nya HTML i domen
+-- Återupprepa
 
-
--- MAIN
+-- MAIN, Visar det i dommen genom...
+-- Init som initiera applikationen
+-- View som visar allt i domen
+-- User inputs skickar till update funktionen
 
 
 main =
@@ -21,7 +30,8 @@ main =
 
 
 
--- MODEL
+-- MODEL Data modeling. Alla detaljer i applikationen som data
+-- Sätter model till int och initierar model med numret 0.
 
 
 type alias Model = Int
@@ -33,12 +43,14 @@ init =
 
 
 
--- UPDATE
+-- UPDATE, beskriver hur vår model förändras beroende på vilka meddelanden den får.
+-- update: Msg -> Model -> Model ->, Tar msg och uppdaterar model och visar ny model i domen
 
 
 type Msg
   = Increment
   | Decrement
+  | Reset
 
 
 update : Msg -> Model -> Model
@@ -50,9 +62,15 @@ update msg model =
     Decrement ->
       model - 1
 
+    Reset ->
+       0
 
 
--- VIEW
+
+-- VIEW, Skriver ut i domen.
+-- Funktion som tar in Model som argument (data-detaljer, int i det här fallet)
+-- Output är i HTML för att skriva ut i domen
+-- view: MOdel -> Html Msg innebär att vi skickar view model = till update som msg typ alltså user-input typ
 
 
 view : Model -> Html Msg
@@ -61,4 +79,5 @@ view model =
     [ button [ onClick Decrement ] [ text "-" ]
     , div [] [ text (String.fromInt model) ]
     , button [ onClick Increment ] [ text "+" ]
+    , button [ onClick Reset ] [ text "Reset" ]
     ]
